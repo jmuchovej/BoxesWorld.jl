ee395519eb1dc194034984b782326f7d2e8717ed
Documenter.jl <documenter@juliadocs.github.io>
2023-06-13 20:45:32 +0000

    build based on 1611711

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoxWorld","category":"page"},{"location":"#BoxWorld","page":"Home","title":"BoxWorld","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoxWorld.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoxWorld]","category":"page"}]
+}
