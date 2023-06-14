e5efe0a9755eca81b2794bc9ce264630af239bbc
Documenter.jl <documenter@juliadocs.github.io>
2023-06-14 15:49:50 +0000

    build based on 6c30c36

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoxWorld","category":"page"},{"location":"#BoxWorld","page":"Home","title":"BoxWorld","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoxWorld.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoxWorld]","category":"page"}]
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BoxesWorld","category":"page"},{"location":"#BoxesWorld","page":"Home","title":"BoxesWorld","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BoxesWorld.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BoxesWorld]","category":"page"}]
 }
